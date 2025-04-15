41e367e38acaf6dc6514ba8a7339e4bb6bfac456
Anastasiya Shulepova <aashulepova@avito.ru>
2025-04-15 23:16:44 +0300

    fix

--- backend/expensewriter/src/main/java/org/ivangalochkin/expensewriter/kafka/config/KafkaCreateBillConsumerConfig.java
+++ backend/expensewriter/src/main/java/org/ivangalochkin/expensewriter/kafka/config/KafkaCreateBillConsumerConfig.java
@@ -3,6 +3,7 @@ package org.ivangalochkin.expensewriter.kafka.config;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.ivangalochkin.expensewriter.proto.CreateBillProto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
